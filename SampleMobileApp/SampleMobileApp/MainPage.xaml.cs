92fad957480c99ac8e5ac42e64c68a31bc3fc4d9
erikur <erick@actual-training.com>
2018-08-27 10:37:07 +0700

    Add project files.

---
+++ SampleMobileApp/SampleMobileApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleMobileApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            btnAdd.Clicked += BtnAdd_Clicked;
+		}
+
+        private void BtnAdd_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Keterangan", "Anda memilih tombol Add", "OK");
+        }
+    }
+}
