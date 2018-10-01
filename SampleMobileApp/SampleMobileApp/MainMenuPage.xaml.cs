61d19a1ae22de5e5aaaaefd138e3c80b646bb934
ekurniawan <erick.kurniawan@gmail.com>
2018-10-01 09:35:14 +0700

    menambahkan menu

--- SampleMobileApp/SampleMobileApp/MainMenuPage.xaml.cs
+++ SampleMobileApp/SampleMobileApp/MainMenuPage.xaml.cs
@@ -17,6 +17,26 @@ namespace SampleMobileApp
             InitializeComponent();
             btnSimpleList.Clicked += BtnNavigasi_Clicked;
             btnTextList.Clicked += BtnNavigasi_Clicked;
+            menuGrid.Clicked += MenuNavigation_Clicked;
+            menuView.Clicked += MenuNavigation_Clicked;
+            menuLuas.Clicked += MenuNavigation_Clicked;
+        }
+
+        private void MenuNavigation_Clicked(object sender, EventArgs e)
+        {
+            var menu = sender as ToolbarItem;
+            if (menu.Text == "Grid Layout")
+            {
+                Navigation.PushAsync(new GridLayoutPage());
+            }
+            else if(menu.Text== "Sample View")
+            {
+                Navigation.PushAsync(new SampleViewPage());
+            }
+            else
+            {
+                Navigation.PushAsync(new LuasSegitigaPage());
+            }
         }
 
         private void BtnNavigasi_Clicked(object sender, EventArgs e)
