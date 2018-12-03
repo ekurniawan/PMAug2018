2d0cd0a9514302d6a31b68e632d48a88193cb21f
ekurniawan <erick.kurniawan@gmail.com>
2018-12-03 09:49:31 +0700

    menambahkan GetAll

---
+++ SampleMobileApp/SampleMobileApp/Services/TodoItemServices.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using SampleMobileApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMobileApp.Services
+{
+    public class TodoItemServices
+    {
+        private HttpClient _client;
+        public List<TodoItem> Items { get; set; }
+        public TodoItemServices()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<List<TodoItem>> GetAllData()
+        {
+            Items = new List<TodoItem>();
+            var uri = new Uri(Helpers.RestUrl);
+            try
+            {
+                var response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
+            return Items;
+        }
+    }
+}
