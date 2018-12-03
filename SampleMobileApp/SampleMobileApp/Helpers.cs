3d4c59d2be6165b42c6624e6c8ac2bc325641a40
ekurniawan <erick.kurniawan@gmail.com>
2018-12-03 09:18:57 +0700

    menambahkan model dan helper class

---
+++ SampleMobileApp/SampleMobileApp/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleMobileApp
+{
+    public static class Helpers
+    {
+        public static string RestUrl = 
+            @"https://actservices.azurewebsites.net/api/TodoItem";
+    }
+}
